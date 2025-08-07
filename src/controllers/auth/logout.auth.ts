4d710aff506178e2eb4f2889624f6adbce843d1c
Milki Legesse <nitfanono@gmail.com>
2025-08-07 15:07:19 +0300

    Logout issue

--- src/controllers/auth/logout.auth.ts
+++ src/controllers/auth/logout.auth.ts
@@ -5,8 +5,8 @@ import { removeAuthCookie } from "./auth.js"
 const authLogoutRouter = express.Router();
 
 authLogoutRouter.post('/', async (req, res) => {
-    const response = res.json({success : true});
     removeAuthCookie(res);
+    const response = res.json({success : true});
     return response;
 })
 
